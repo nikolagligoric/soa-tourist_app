2f31ab4a8ad5a78edc8d0e0ea6bfcd9fb99f873b
Danijela Medic <medic.danijeladaca@gmail.com>
2026-04-11 23:43:40 +0200

    feat/blog-create: REST - implement blog creation with image upload and database integration

---
+++ Blog/blog-be/Blog.Application/DTOs/BlogCreatedDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.DTOs
+{
+    public class BlogCreatedDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public string AuthorUsername { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = new();
+    }
+}
---
+++ Blog/blog-be/Blog.Application/Interfaces/IBlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Domain.Entities;
+
+namespace Blog.Application.Interfaces
+{
+    public interface IBlogRepository
+    {
+        Blog.Domain.Entities.Blog Add(Blog.Domain.Entities.Blog blog);
+    }
+}
