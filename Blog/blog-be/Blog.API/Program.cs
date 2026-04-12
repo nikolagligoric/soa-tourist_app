cfb542073228586dc61f5bf526544470c41dc79c
Danijela Medic <medic.danijeladaca@gmail.com>
2026-04-12 02:10:37 +0200

    feat/blog-create: DOCKER - implemented blog creation and dockerize service

--- Blog/blog-be/Blog.API/Program.cs
+++ Blog/blog-be/Blog.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -72,6 +73,12 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
