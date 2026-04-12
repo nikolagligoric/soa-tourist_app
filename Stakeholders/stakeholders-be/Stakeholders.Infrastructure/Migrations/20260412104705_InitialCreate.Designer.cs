830c37c85095ca0235399611a7331bd4f5688bec
Nikola Gligoric RA 6/2022 <nikolagligoric33@gmail.com>
2026-04-12 13:00:14 +0200

    fix: Fixed docker-compose.yml

--- Stakeholders/stakeholders-be/Stakeholders.Infrastructure/Migrations/20260412104705_InitialCreate.Designer.cs
+++ Stakeholders/stakeholders-be/Stakeholders.Infrastructure/Migrations/20260412104705_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Stakeholders.Infrastructure.Persistence;
 namespace Stakeholders.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260411000446_InitialCreate")]
+    [Migration("20260412104705_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
