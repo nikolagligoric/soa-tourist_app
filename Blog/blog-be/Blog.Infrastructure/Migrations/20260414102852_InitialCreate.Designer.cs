87ce47b67ebc08d7c40c5e5029ee87dbd53dc48b
Bojan Grujicic <bojangrujicic5@gmail.com>
2026-04-14 12:46:47 +0200

    fix

--- Blog/blog-be/Blog.Infrastructure/Migrations/20260414102852_InitialCreate.Designer.cs
+++ Blog/blog-be/Blog.Infrastructure/Migrations/20260414102852_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Blog.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260414023524_InitialCreate")]
+    [Migration("20260414102852_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
